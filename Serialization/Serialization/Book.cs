d0fab5b2ec69bc4c953b38e243cac2a0f3f26be6
Alina Apet <Alina_Apet@epam.com>
2018-03-15 16:04:03 +0300

    Serialization util template

---
+++ Serialization/Serialization/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+	[Serializable, XmlType(TypeName = "book")]
+	public class Book
+	{
+		private DateTime publishDate, registrationDate; 
+
+		[XmlAttribute("id")]
+		public string Id { get; set; }
+
+		[XmlElement("isbn")]
+		public string Isbn { get; set; }
+
+		[XmlElement("author")]
+		public string Author { get; set; }
+
+		[XmlElement("title")]
+		public string Title { get; set; }
+
+		[XmlElement("genre")]
+		public GenreType Genre { get; set; }//must be enum
+
+		[XmlElement("publisher")]
+		public string Publisher { get; set; }
+
+		[XmlElement("publish_date")]
+		public DateTime PublishDate
+		{
+			get
+			{
+				return publishDate.Date;
+			}
+			set { publishDate = value; }
+		}
+
+
+		[XmlElement("registration_date")]
+		public DateTime RegistrationDate
+		{
+			get
+			{
+				return registrationDate.Date;
+			}
+			set { registrationDate = value; }
+		}
+
+
+		[XmlElement("description")]
+		public string Description { get; set; }
+
+		public Book()
+		{
+
+		}
+
+		public override string ToString()
+		{
+			return $"Book:{Title},  Author {Author}\n" +  $"{Genre}. {Description}" +  $"{PublishDate}, {Publisher}\n";
+		}
+
+		 
+	}
+}
---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+	[Serializable]
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// an object to be serialized
+			Book book = new Book();
+			Console.WriteLine("Object was created");
+
+			// sending class type to constructor
+			XmlSerializer formatter = new XmlSerializer(typeof(Book));
+
+			// get the stream to write serialized object in
+			using (FileStream fs = new FileStream("books.xml", FileMode.OpenOrCreate))
+			{
+				formatter.Serialize(fs, book);
+
+				Console.WriteLine("Object was serialized");
+			}
+
+			// deserialization
+			using (FileStream fs = new FileStream("books.xml", FileMode.OpenOrCreate))
+			{
+				var newBook = (Book)formatter.Deserialize(fs);
+
+				Console.WriteLine("Object was deserialized");
+				Console.WriteLine("AuthName: {0} --- Title: {1}", newBook.Author, newBook.Title);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
